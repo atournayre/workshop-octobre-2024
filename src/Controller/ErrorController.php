ab89e655019e3bda6a7e7b5a9f0990afb73398d4
Aurélien Tournayre <aurelien.tournayre@gmail.com>
2024-10-20 23:05:05 +0200

    Ajout de méthodes pour les pages d'erreur

--- src/Controller/ErrorController.php
+++ src/Controller/ErrorController.php
@@ -5,10 +5,18 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
 
 final class ErrorController extends AbstractController
 {
-    public function __invoke(): Response
+    #[Route('/non-trouve', name: 'app_error_non_trouve')]
+    public function nonTrouve(): Response
+    {
+        return new Response('La page demandée n\'existe pas', Response::HTTP_NOT_FOUND);
+    }
+
+    #[Route('/server', name: 'app_error_server')]
+    public function server(): Response
     {
         return new Response('Oups, une erreur est survenue.', Response::HTTP_INTERNAL_SERVER_ERROR);
     }
